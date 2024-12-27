6f72a6e1d734f98f4e749270cbf8fe828a8ca412
Wimwipa780 <wimwipha4746@gmail.com>
2024-12-27 17:28:54 +0700

    Lab13_1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,31 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double max = A[0],min = A[0];
+    double sum = 0;
+    double sumHarmo = 0;
+    double ruom = 1;
+    for(int i = 0; i<N; i++){
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+        sumHarmo +=  1/A[i];
+        sum += A[i];
+        ruom*= A[i];
+    }
+    
+    B[0] = sum/N;
+    
+    double sumsum = 0 ;
+    for(int j = 0; j<N;j++){
+      sumsum += pow((A[j]-B[0]),2);
+    }
+   
+
+    B[1] = sqrt(sumsum/N);
+    B[2] = pow(ruom,1.0/N);
+    B[3] = N/sumHarmo;
+    B[4] = max;
+    B[5] = min;
+}
